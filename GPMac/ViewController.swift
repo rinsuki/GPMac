edc2dec77a555836dadc48b0c4d4c44acb6401ef
rinsuki <428rinsuki+git@gmail.com>
2019-12-06 21:37:17 +0900

    Initial Commit

---
+++ GPMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GPMac
+//
+//  Created by user on 2019/12/06.
+//  Copyright © 2019 rinsuki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
